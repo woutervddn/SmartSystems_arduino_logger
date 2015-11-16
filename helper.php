4a818be4416bda8728b72b12d13988f8748203e1
Wouter Vandenneucker <wouter@vandenneucker.be>
2015-11-16 12:05:32 +0100

    changes

--- helper.php
+++ helper.php
@@ -1,13 +1,14 @@
 <?php
-
+function getHello(){
+	return "bonkers";
+}
 
 function writeToDb($sensorName, $sensorVal){
 	$servername = "localhost";
-	$username = "***";
-	$password = "***";
-	$dbname = "***";
-	$table = "***";
-
+	$username = "pxleai1q_1402985";
+	$password = "rU6)$]cIVV_9";
+	$dbname = "pxleai1q_1402985";
+	$table = "smartsystems";
 	try {
 	    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 	    // set the PDO error mode to exception
@@ -16,6 +17,33 @@ function writeToDb($sensorName, $sensorVal){
 	    // use exec() because no results are returned
 	    $conn->exec($sql);
 	    return "New record created successfully";
+	}
+	catch(PDOException $e){
+	    return $sql . "<br>" . $e->getMessage();
+	}
+	$conn = null;
+}
+
+function getFromDb($sensorName){
+	$servername = "localhost";
+	$username = "pxleai1q_1402985";
+	$password = "rU6)$]cIVV_9";
+	$dbname = "pxleai1q_1402985";
+	$table = "smartsystems";
+	
+	try {
+	    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    $getData = $conn->prepare("SELECT * FROM $table WHERE sensor_name='$sensorName'");
+		$getData->execute();
+		$data = $getData->fetchAll();
+
+		return $data;
+	    //$sql = "SELECT * FROM $table WHERE sensor_name='$sensorName'";
+	    // use exec() because no results are returned
+	    //$conn->exec($sql);
+	    //return $sql;
 	}
 	catch(PDOException $e){
 	    return $sql . "<br>" . $e->getMessage();
--- index.php
+++ index.php
@@ -8,14 +8,46 @@ $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
 
+$app->get('/ss/dashboard', function () {
+    // Displays the dashboard
+    echo"<html><head><script type='text/javascript' src=\"https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1','packages':['corechart']}]}\"></script><script type='text/javascript'>google.setOnLoadCallback(drawCharts);";
+    echo "function drawCharts() {";
+    echo "var data = google.visualization.arrayToDataTable([['Time', 'Temperature Value'],";
+
+	  $data = getFromDb('temperature');
+	  $notFirst = false;
+	  foreach($data as $key => $datapoint){
+	  	if( $notFirst ){ echo ","; }
+	  	$notFirst = true;
+	  	echo "['" . $datapoint['timestamp'] . "', " . $datapoint['sensor_value'] . "]";
+	  }
+	echo "]);var options = {title: 'Temperature Sensor',curveType: 'function',legend: { position: 'bottom' }};var chart = new google.visualization.LineChart(document.getElementById('temp_chart'));chart.draw(data, options);";
+
+	echo "var data = google.visualization.arrayToDataTable([['Time', 'Light Value'],";
+
+	  $data = getFromDb('lichtsensor');
+	  $notFirst = false;
+	  foreach($data as $key => $datapoint){
+	  	if( $notFirst ){ echo ","; }
+	  	$notFirst = true;
+	  	echo "['" . $datapoint['timestamp'] . "', " . $datapoint['sensor_value'] . "]";
+	  }
+	echo "]);var options = {title: 'Light Sensor',curveType: 'function',legend: { position: 'bottom' }};var chart = new google.visualization.LineChart(document.getElementById('licht_chart'));chart.draw(data, options);";
+	
+	echo "}</script></head>";
+	echo "<body><div id='temp_chart' style='width: 900px; height: 500px'></div><br/><div id='licht_chart' style='width: 900px; height: 500px'></div></body></html>";
+
+
+});
+
 $app->post('/ss/temperature/:value', function ($value) {
-    //echo "Temperature value: $value °C";
+    // Does some magic specifically for the temperature sensor.
     $temp = floatval ( intval($value) / 100 );
     echo writeToDb('temperature', $temp);
 });
 
 $app->post('/ss/:sensor/:value', function ($sensor, $value) {
-    //echo "Temperature value: $value °C";
+    // Works for any sensor
     echo writeToDb($sensor, $value);
 });
 
